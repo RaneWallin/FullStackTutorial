b2aad6bca60a618131526cbc35f95846a7b44345
EC2 Default User <ec2-user@ip-172-31-26-8.us-east-2.compute.internal>
2019-04-21 12:35:44 +0000

    update package.json

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+// app is the express server
+// .get() creates a new route handler
+// "/"" is the route
+// req  (request) - incoming request
+// res (response) - the response that is being send back
+// res.send - close request and send back response with json data
 app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-app.listen(8080);
+// get port dynamically from environment variable or use 8080 by default
+const PORT = process.env.PORT || 8080;
+
+// instructs express to tell node to listen to incoming traffic on provided
+// port 
+app.listen(PORT);
+
+
+// Express methods: get (get info), post(update property), 
+// put (add new or update all properties), delete (remove), 
+// patch (update multiple properties)
