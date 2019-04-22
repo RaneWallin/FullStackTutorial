8a872489b8f1e9e31467b54352f5ce7640de7156
ranewallin <github@blasph.me>
2019-04-22 14:47:34 -0700

    querying/creating users after authentication

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
-require('./models/user');
 
 // connect to mongoose and pass the provided URI
 mongoose.connect(keys.mongoURI);
 
-
+// thing
 const app = express();
 
 //
--- services/passport.js
+++ services/passport.js
@@ -1,21 +1,30 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+// get the users model from mongoose
+const User = mongoose.model("users");
 
 // Telling passport what strategy we want to use for authentication
 // GoogleStrategy(configurationOptions, function)
 passport.use(
-    new GoogleStrategy({
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log("access token",
-                accessToken,
-                "refresh token",
-                refreshToken,
-                "profile",
-                profile);
-        })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (!existingUser) {
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser));
+        } else {
+          done(null, existingUser);
+        }
+      });
+    }
+  )
 );
