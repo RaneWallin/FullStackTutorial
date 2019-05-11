8751a9f7685a2494395c98bcaa1010b985faf02c
RaneWallin <rane@ranewallin.com>
2019-05-11 14:21:02 -0700

    conditional rendering of login link

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,10 +3,10 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
-  const response = await axios.get("/api/current_user");
+  const res = await axios.get("/api/current_user");
 
-  dispatch({
+  return dispatch({
     type: FETCH_USER,
-    payload: response
+    payload: res.data
   });
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,14 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+const initialState = {
+  user: null
+};
+
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+    //return { ...state, user };
     default:
       return state;
   }
