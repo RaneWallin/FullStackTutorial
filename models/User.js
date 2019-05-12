eae45e7f0b51699ce8d928b69c4a72e02be4083f
RaneWallin <rane@ranewallin.com>
2019-05-11 17:32:31 -0700

    update user with credits

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // MongoDB does not require a schema, but mongoose does. Set the
 // Scehema object here
 const userSchema = new Schema({
-    googleId: String,
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
-
 // Tell mongodb to create a model named users
-mongoose.model('users', userSchema);
+mongoose.model("users", userSchema);
