be936806d5d6725fdf79d0ea74929d95270acccc
RaneWallin <rane@ranewallin.com>
2019-05-11 17:55:26 -0700

    add requireLogin middleware and verify user is logged in before processing payment.

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,9 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
@@ -10,8 +11,16 @@ module.exports = app => {
       source: req.body.id
     });
 
-    req.user.credits += 5;
-    const user = await req.user.save();
-    res.status(202).json(user);
+    if (charge.status != "succeeded")
+      return res.status(402).json({ error: "Payment failed." });
+
+    try {
+      req.user.credits += 5;
+      const user = await req.user.save();
+
+      return res.status(202).json(user);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
 };
