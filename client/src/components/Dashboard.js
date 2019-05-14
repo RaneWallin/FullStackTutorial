311dba2fbeea9ce143249f6c2b3119347b53443b
RaneWallin <rane@ranewallin.com>
2019-05-14 14:14:06 -0700

    scaffold dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div style={{ border: "1px solid black" }}>
+      Dashboard
+      <div
+        className="fixed-action-btn"
+        style={{ marginRight: "30px", marginBottom: "30px" }}
+      >
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="large material-icons">add</i>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
